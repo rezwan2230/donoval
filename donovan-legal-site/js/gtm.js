ad3dce93c4485017362877d7e92434089de549aa
Rezwan <rezwan2230@gmail.com>
2026-09-15 11:41:31 +0600

    add gtm

--- donovan-legal-site/js/gtm.js
+++ donovan-legal-site/js/gtm.js
@@ -4,6 +4,9 @@ const containerId = 'GTM-W9DH8BN6';
 const dataLayerName = 'dataLayer';
 const dataLayer = window[dataLayerName] = window[dataLayerName] || [];
 
+if (dataLayer.some((entry) => entry && entry.event === 'gtm.js')) {
+	// The homepage has the standard detector-friendly snippet in its head.
+} else {
 dataLayer.push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
 
 const firstScript = document.getElementsByTagName('script')[0];
@@ -13,3 +16,4 @@ const layerQuery = dataLayerName === 'dataLayer' ? '' : `&l=${dataLayerName}`;
 gtmScript.async = true;
 gtmScript.src = `https://www.googletagmanager.com/gtm.js?id=${containerId}${layerQuery}`;
 firstScript.parentNode.insertBefore(gtmScript, firstScript);
+}
